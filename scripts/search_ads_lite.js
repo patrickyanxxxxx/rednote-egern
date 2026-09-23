92e76b9c702dfb93b58d5c9acd5be6c651ff8e71
patrickyanxxxxx <150406461+patrickyanxxxxx@users.noreply.github.com>
2026-09-23 21:22:14 +0800

    Filter search ads without blocking normal xhslink URLs

--- scripts/search_ads_lite.js
+++ scripts/search_ads_lite.js
@@ -5,15 +5,18 @@ function isObject(value) {
 export default async function(ctx) {
   try {
     const data = await ctx.response.json();
-    if (/\/search\/banner_list(?:\?|$)/.test(ctx.request?.url || "")) {
+    const url = ctx.request?.url || "";
+    if (/\/search\/notes(?:\?|$)/.test(url) && Array.isArray(data?.data?.items)) {
+      data.data.items = data.data.items.filter(item => item?.model_type !== "ads");
+    } else if (/\/search\/banner_list(?:\?|$)/.test(url)) {
       if (data?.data !== undefined) data.data = {};
-    } else if (/\/search\/(?:hot_list|trending)(?:\?|$)/.test(ctx.request?.url || "")) {
+    } else if (/\/search\/(?:hot_list|trending)(?:\?|$)/.test(url)) {
       if (Array.isArray(data?.data?.items)) data.data.items = [];
       if (isObject(data?.data)) {
         data.data.queries = [];
         data.data.hint_word = {};
       }
-    } else if (/\/search\/hint(?:\?|$)/.test(ctx.request?.url || "") && isObject(data?.data)) {
+    } else if (/\/search\/hint(?:\?|$)/.test(url) && isObject(data?.data)) {
       data.data.hint_words = [];
     }
     return { body: data };
